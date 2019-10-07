72e7bd9dd928a5fa56cdfc53dc5d097b15f72989
adam <ibrahimadam193@gmail.com>
2019-10-07 00:40:40 -0400

    Use body-parser, create JSON validation middleware
    Many API endpoints will expect JSON data, and will require certain
    fields. The validator will check for the existence of those fields and
    handle sending error responses if they're missing.
    
    Changes to be committed:
          modified:   backend/app.js

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
-'use strict'
+'use strict';
 
 const express = require('express');
-//const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const models = require('./models')
 const jwt = require('jsonwebtoken');
 const PORT = process.env.PORT || 8000;
@@ -13,6 +13,36 @@ app.use((req, res, next) => {
         "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
+
+/**
+ * Given a list of field names, will check the request body of a JSON
+ * request to see if all of the fields are there. If not, will report
+ * the missing fields and give a 400 error response.
+ *
+ * Assumes bodyParser middleware already in place.
+ */
+function validateJSONFields(fields) {
+    return (req, res, next) => {
+        if (req.header['content-type'] !== 'application/json') {
+            next();
+        }
+        console.log('json validator:', req.body);
+        let missingFields = [];
+        for (let field of fields) {
+            if (req.body[field] === undefined) {
+                missingFields.push(field);
+            }
+        }
+        if (missingFields.length > 0) {
+            res.status(400).send("Missing JSON fields in request: " +
+                missingFields.join(", "));
+        }
+    };
+}
+
+// This will only parse when the request's Content-Type is
+// application/json
+app.use(bodyParser.json());
 
 //FIXME: Currently, both back and frontend have a preshared
 //secret which is stored in the source of each file.

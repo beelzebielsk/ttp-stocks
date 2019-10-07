2fc86eba8949a15018bac47eea616dc415464e5b
adam <ibrahimadam193@gmail.com>
2019-10-07 18:39:59 -0400

    Write form validators, make messages more standard
    Changes to be committed:
          modified:   frontend/my-app/src/components/form-validators.js

--- frontend/my-app/src/components/form-validators.js
+++ frontend/my-app/src/components/form-validators.js
@@ -3,17 +3,43 @@
  * object has fields 'success' and 'reason'. If the entry is valid,
  * then return { success: true }. If the entry is invalid, then return
  * {success: false, reason: why-is-invalid}.
+ *
+ * All of these take text, the current value of some form input.
  */
 export function validateEmail(email) {
     if (email === '') {
-        return {success: false, reason: "No email entered!"};
+        return {success: false, reason: "Email field is empty"};
     }
     return {success: true};
 }
 
 export function validatePassword(password) {
     if (password === '') {
-        return {success: false, reason: "No password entered!"};
+        return {success: false, reason: "Password field is empty"};
+    }
+    return {success: true};
+}
+
+export function validateTickerName(tickerName) {
+    if (tickerName === '') {
+        return {success: false, reason: "Ticker field is empty"};
+    }
+    return {success: true};
+}
+
+export function validatePositiveInteger(number) {
+    if (number === '') {
+        return {success: false, reason: "Number field is empty"};
+    }
+    let parsed = Number(number);
+    if (isNaN(parsed)) {
+        return {success: false, reason: "Invalid number entered"};
+    }
+    if (parsed <= 0) {
+        return {success: false, reason: "Nonpositive number entered"};
+    }
+    if (!Number.isInteger(parsed)) {
+        return {success: false, reason: "Decimal number entered"};
     }
     return {success: true};
 }

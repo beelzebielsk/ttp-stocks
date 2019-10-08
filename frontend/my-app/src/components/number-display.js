5b89373b69813403526be52a26bddce54d73e741
adam <ibrahimadam193@gmail.com>
2019-10-07 22:53:09 -0400

    Put precision control in Currency and Percent
    Changes to be committed:
          modified:   frontend/my-app/src/components/number-display.js

--- frontend/my-app/src/components/number-display.js
+++ frontend/my-app/src/components/number-display.js
@@ -1,12 +1,18 @@
 'use strict';
 import React from 'react';
 
+/* Both of these functions take a single number as their children
+ */
 // TODO: Put in the correct precision for money.
 export function Currency(props) {
-    return <span>${props.children}</span>;
+    const num = props.children;
+    const rounded = num.toFixed(2);
+    return <span>${rounded}</span>;
 }
 
 // TODO: Put in sensible precision for percent.
 export function Percent(props) {
-    return <span>%{props.children}</span>;
+    const num = props.children;
+    const rounded = num.toFixed(3);
+    return <span>%{rounded}</span>;
 }

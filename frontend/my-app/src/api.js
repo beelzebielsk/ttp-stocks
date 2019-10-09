233f269c0012f11734bbf4dc2ca3c64e215a9722
adam <ibrahimadam193@gmail.com>
2019-10-09 02:57:56 -0400

    Include auth header in fetchBackend if logged in
    On branch backend-work
    Changes to be committed:
          modified:   frontend/my-app/src/api.js

--- frontend/my-app/src/api.js
+++ frontend/my-app/src/api.js
@@ -7,7 +7,19 @@ const API_URL='http://localhost:8000';
  * @param {object} options - fetch options to forward to fetchBackend
  */
 export async function fetchBackend(url, options) {
-    return fetch(`${API_URL}${url}`, options);
+    let headers = {};
+    options = options || {};
+    if ('token' in localStorage) {
+        headers['Authorization'] = `Bearer ${localStorage.token}`;
+    }
+    let optionsCopy = {...options};
+    if (options.headers === undefined) {
+        optionsCopy.headers = headers;
+
+    } else {
+        optionsCopy.headers = {...headers, ...options.headers};
+    }
+    return fetch(`${API_URL}${url}`, optionsCopy);
 }
 
 /* Handles any extra steps necessary to send JSON to the backend. Any

234eacf076e3e018d78b814543c1895a3b9920eb
adam <ibrahimadam193@gmail.com>
2019-10-09 03:21:13 -0400

    Stop verify-token lop off 1st char of token
    On branch backend-work
    Changes to be committed:
          modified:   backend/middlewares/verify-token.js

--- backend/middlewares/verify-token.js
+++ backend/middlewares/verify-token.js
@@ -10,18 +10,18 @@ const jwt = require('jsonwebtoken');
 module.exports = function verifyJWTToken(secretOrPublicKey) {
     return (req, res, next) => {
         console.log("verifying jwt token");
-        if (req.header['authorization'] === undefined) {
+        if (req.header('Authorization') === undefined) {
             console.log("No auth header.");
             res.status(401).send("Need to present authorization token.");
             return;
         }
         console.log("Checking auth header contents");
-        const auth = req.header['authorization'];
+        const auth = req.header('authorization');
         if (!auth.startsWith("Bearer")) {
             res.status(400).send("Authorization header needs to start with 'Bearer'.");
             return;
         }
-        const token = auth.slice('Bearer '.length + 1);
+        const token = auth.slice('Bearer '.length);
         try {
             const decoded = jwt.verify(token, secretOrPublicKey);
             req.credentials = decoded;

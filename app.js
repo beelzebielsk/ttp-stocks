900ce3097d44d7ff4552616917ef498d5bb1c127
adam <ibrahimadam193@gmail.com>
2019-10-24 00:56:30 -0400

    Add static serve of fronend, frontend build script
    On branch fix-for-production
    Changes to be committed:
          modified:   app.js
          modified:   package.json

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const allowCors = require('./middlewares/cors');
 const models = require('./models')
 const api_router = require('./api-router');
 const path = require('path');
+const env = process.env.NODE_ENV || 'development';
 
 const PORT = process.env.PORT || 8000;
 const app = express()
@@ -16,6 +17,14 @@ app.use((req, res, next) => {
 });
 app.use(allowCors);
 app.use('/api', api_router);
+
+
+if (env === 'production') {
+    app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+    app.get('/*', async (req, res) => {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    });
+}
 
 models.sequelize.sync({force: false})
     .then(() => {

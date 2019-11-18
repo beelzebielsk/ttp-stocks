e80e8d87b783a777d047d5602244f223ccb0c9a1
adam <ibrahimadam193@gmail.com>
2019-11-18 13:21:11 -0500

    hard to refactor into handleNoValidToken
    There is a lot of duplication of work in the way I tried to refactor.
    The error handling code has no way to figure out what went wrong, so it
    would have to reproduce all the steps of verifyJWTToken as it was
    originally written to reproduce the problems. I prevented functions from
    giving detailed error messages, so that hamstrung my error handler.
    
    It may be better to just say "getDecodedToken" and let the errors from
    getDecodedToken specify what went wrong, and for now handle all of the
    errors in one place.

--- middlewares/verify-token.js
+++ middlewares/verify-token.js
@@ -32,18 +32,13 @@ module.exports = function verifyJWTToken(secretOrPublicKey) {
 
 function verifyJWTToken(secretOrPublicKey) {
     return (req, res, next) => {
-        try {
-            validateAuthHeader(req);
-        } catch (err) {
-            if (err.message === "No Authorization header on request.") {
-                handleAbsentHeader(req, res);
-            } else if (err.message === "Authorization header is malformed: should start with 'Bearer'.") {
-                handleMalformedHeader(req, res);
-            }
-            return;
+        let token;
+        if (validTokenExists(req, secretOrPublicKey)) {
+            token = getDecodedToken(req, secretOrPublicKey);
+            req.credentials = token;
+        } else {
+            handleNoValidToken()
         }
-
-        const token = extractToken(req);
 
         try {
             const decoded = jwt.verify(token, secretOrPublicKey);
@@ -59,6 +54,44 @@ function verifyJWTToken(secretOrPublicKey) {
     }
 }
 
+// this should return true if one exists, and false otherwise. That is
+// all it should do. Don't worry for now that determining validity
+// means getting or failing to decode the token.
+function validTokenExists(req, secretOrPublicKey) {
+    try {
+        validateAuthHeader(req);
+        const token = extractTokenFromHeaders(req);
+        const decoded = jwt.verify(token, secretOrPublicKey);
+    } catch (err) {
+        return false;
+    }
+}
+
+function getDecodedToken(req, secretOrPublicKey) {
+    const token = extractTokenFromHeaders(req);
+    return jwt.verify(token, secretOrPublicKey);
+}
+
+function getToken(req) {
+    try {
+        validateAuthHeader(req);
+    } catch (err) {
+        if (err.message === "No Authorization header on request.") {
+            handleAbsentHeader(req, res);
+        } else if (err.message === "Authorization header is malformed: should start with 'Bearer'.") {
+            handleMalformedHeader(req, res);
+        }
+        return;
+    }
+
+    const token = extractToken(req);
+
+}
+
+function extractTokenFromHeaders(req) {
+    return req.header('authorization').slice('Bearer '.length);
+}
+
 function validateAuthHeader(req) {
     console.log("verifying jwt token");
     if (req.header('authorization') === undefined) {
@@ -70,11 +103,6 @@ function validateAuthHeader(req) {
     if (!auth.startsWith("Bearer")) {
         throw new Error("Authorization header is malformed: should start with 'Bearer'.")
     }
-}
-
-function extractToken(req) {
-    const token = req.header('authorization').slice('Bearer '.length);
-    return token;
 }
 
 function handleAbsentHeader(req, res) {
@@ -94,5 +122,5 @@ function handleInvalidToken(req, res) {
  * - extract the token from the request
  * - decode the token
  *      - handle malformed token
- *
+ */
 

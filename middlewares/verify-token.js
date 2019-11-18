0d7c865fb5369568dc86c78d57117fab4df540d9
adam <ibrahimadam193@gmail.com>
2019-11-18 13:00:54 -0500

    first refactor of verifyJWTToken
    Tried to make functions to explain the steps of what is going on. Some
    stuff I wasn't able to refactor as well.
    
    tried to split up tasks in function like so:
    
    /* tasks done in verifyJWTToken:
     * - validate the header
     *      - does it contain the Authorization header?
     *      - is the authorization header well formed?
     * - extract the token from the request
     * - decode the token
     *      - handle malformed token
     */

--- middlewares/verify-token.js
+++ middlewares/verify-token.js
@@ -29,3 +29,70 @@ module.exports = function verifyJWTToken(secretOrPublicKey) {
         }
     }
 }
+
+function verifyJWTToken(secretOrPublicKey) {
+    return (req, res, next) => {
+        try {
+            validateAuthHeader(req);
+        } catch (err) {
+            if (err.message === "No Authorization header on request.") {
+                handleAbsentHeader(req, res);
+            } else if (err.message === "Authorization header is malformed: should start with 'Bearer'.") {
+                handleMalformedHeader(req, res);
+            }
+            return;
+        }
+
+        const token = extractToken(req);
+
+        try {
+            const decoded = jwt.verify(token, secretOrPublicKey);
+            req.credentials = decoded;
+            next();
+        } catch (err) {
+            if (err.name === 'JsonWebTokenError') {
+                handleInvalidToken(req, res)
+            } else {
+                next(err);
+            }
+        }
+    }
+}
+
+function validateAuthHeader(req) {
+    console.log("verifying jwt token");
+    if (req.header('authorization') === undefined) {
+        console.log("No auth header.");
+        throw new Error("No Authorization header on request.");
+    } 
+    console.log("Checking auth header contents");
+    const auth = req.header('authorization');
+    if (!auth.startsWith("Bearer")) {
+        throw new Error("Authorization header is malformed: should start with 'Bearer'.")
+    }
+}
+
+function extractToken(req) {
+    const token = req.header('authorization').slice('Bearer '.length);
+    return token;
+}
+
+function handleAbsentHeader(req, res) {
+    res.status(401).send("Need to present authorization token.");
+}
+function handleMalformedHeader(req, res) {
+    res.status(400).send("Authorization header needs to start with 'Bearer'.");
+}
+function handleInvalidToken(req, res) {
+    res.status(401).send("Invalid token submitted.");
+}
+
+/* tasks done in verifyJWTToken:
+ * - validate the header
+ *      - does it contain the Authorization header?
+ *      - is the authorization header well formed?
+ * - extract the token from the request
+ * - decode the token
+ *      - handle malformed token
+ *
+
